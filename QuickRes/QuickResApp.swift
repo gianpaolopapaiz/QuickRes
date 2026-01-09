1c09be0057529d067665d3909d3724694c4267ee
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2026-01-08 21:13:46 -0300

    Initial Commit

---
+++ QuickRes/QuickResApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickResApp.swift
+//  QuickRes
+//
+//  Created by Gianpaolo Papaiz on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
